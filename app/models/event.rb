866d59304dfa01199a9ffa120648460b9309deca
Derric Donehoo <derric.donehoo@gmail.com>
2019-05-27 16:41:15 +0200

    set up controllers, routes and models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,9 @@
 class Event < ApplicationRecord
+  belongs_to :club
+  acts_as_taggable
+
+
+  def event_params
+    params.require(:event).permit(:name, :date, :description, :link, :tag_list) ## Rails 4 strong params usage
+  end
 end
