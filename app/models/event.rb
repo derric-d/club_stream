2d998b150adf4d8f84acbeaedd0aaabfbded3a53
Nina Karas <KarasNina@googlemail.com>
2019-05-27 17:51:28 +0200

    home index

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,7 @@ class Event < ApplicationRecord
   belongs_to :club
   acts_as_taggable
 
+  validates :name, :link, :date, presence: true
 
   def event_params
     params.require(:event).permit(:name, :date, :description, :link, :tag_list) ## Rails 4 strong params usage
