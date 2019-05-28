d5b18441936fc99e108a17e5bb84aa956380191d
Derric Donehoo <derric.donehoo@gmail.com>
2019-05-28 18:13:13 +0200

    done with first scrape

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,19 +1,5 @@
 class EventsController < ApplicationController
-  def find_events
-    require 'nokogiri'
-    require 'open-uri'
 
-    url = 'https://www.residentadvisor.net/club.aspx?id=5031'
-
-    html_file = open(url).read
-    html_doc = Nokogiri::HTML(html_file)
-
-    html_doc.search('#items .list ul').each do |element|
-      text = element.text.strip
-      # puts element.attribute('href').value
-    end
-    return text
-  end
 
   private
 
