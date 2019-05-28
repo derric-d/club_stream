b48844219f8ec48d90eaf2fc00baacc5b97b0657
Nina Karas <KarasNina@googlemail.com>
2019-05-28 17:16:19 +0200

    selector

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:username, :tag_list) ## Rails 4 strong params usage
+    params.require(:user).permit(:username, tag_list: []) ## Rails 4 strong params usage
   end
 end
