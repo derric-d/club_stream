abaaf9d413bebc9bc7e30c3dc206771bf43d1d1d
Stephan Grund <stephan.grund@whu.edu>
2019-05-29 19:11:20 +0200

    message improvement

--- app/jobs/sendmessages_job.rb
+++ app/jobs/sendmessages_job.rb
@@ -8,7 +8,7 @@ class SendmessagesJob < ApplicationJob
         :username => "whudemo1",
         :password => "KdqfQA",
         :sendType => "simple",
-        :text => "#{Event.tagged_with([User.last.tag_list], any: true).last.name}",
+        :text => "🎊 Your <strong>Clubster</strong> selection 🎉 <br> ✅#{Event.tagged_with([User.last.tag_list], any: true).last.name} at #{Event.tagged_with([User.last.tag_list], any: true).last.club} on #{Event.tagged_with([User.last.tag_list], any: true).last.date}, #{Event.tagged_with([User.last.tag_list], any: true).last.url} <br> ✅#{Event.tagged_with([User.first.tag_list], any: true).first.name} at #{Event.tagged_with([User.first.tag_list], any: true).first.club} on #{Event.tagged_with([User.first.tag_list], any: true).first.date}, #{Event.tagged_with([User.first.tag_list], any: true).first.url}  🎧Enjoy your parties🎶",
         :msisdn => "#{user.phone}"
        })
   end
