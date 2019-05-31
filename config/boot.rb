0198c7374b603474db24c9eb91b3bbea18a5a796
Derric Donehoo <derric.donehoo@gmail.com>
2019-05-31 11:34:30 +0200

    try disable bootsnap for Heroku

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,4 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap/setup' unless Rails.env.production?
+require 'bootsnap/setup' unless ENV["RAILS_ENV"] == "production"
