b43845c1e5bab3cf3a836f1c80848f1855c5ae16
Stephan Grund <stephan.grund@whu.edu>
2019-06-03 15:56:43 +0200

    shortener

---
+++ app/services/shortener_service.rb
@@ -0,0 +1,13 @@
+class ShortenerService
+  def initialize(event)
+    @event = event
+  end
+
+  def call
+    @event.link
+    response = HTTP.get "https://api.rebrandly.com/v1/links/new?apikey=#{ENV['REBRANDLY']}&destination=#{@event.link}&domain[fullName]=l.clubster.io"
+    body = JSON.parse(response.to_s)
+    url = body['shortUrl']
+    @event.update!(shorturl: url)
+  end
+end
