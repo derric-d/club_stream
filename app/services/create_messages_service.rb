a79de9661b24675293689ff2925fe92c11db9349
Stephan Grund <stephan.grund@whu.edu>
2019-06-03 18:38:50 +0200

    message

---
+++ app/services/create_messages_service.rb
@@ -0,0 +1,33 @@
+class CreateMessagesService
+
+  def create_message(user_id)
+      user = User.find(user_id)
+      events = Event.tags_for_user(user).limit(5)
+      message = <<~MESSAGE
+  🎧 Your *Clubster* selection for _next_ _days_ 🎧
+
+  #{event_message_builder(events)}
+
+  Change your preferences on www.clubster.io/preferences 🎧
+      MESSAGE
+
+      HTTP.post(
+        "https://gate.appworkx.de/ws/send.php?",
+        json: {
+          username: ENV['WAUSERNAME'],
+          password: ENV['WAPASSWORD'],
+          sendType: "simple",
+          filename: "https://source.unsplash.com/random(800x800)",
+          type: "image",
+          text: message,
+          msisdn: "#{user.phone}"
+         }
+       )
+     end
+
+     def event_message_builder(events)
+       events.map do |event|
+         "✔️ *#{event.name}* at #{event.club_name} on #{event.date.strftime("%a, %b %d")}, #{event.shorturl}"
+       end.join("\n\n")
+     end
+end
