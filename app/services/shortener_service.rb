442f2901fc11046f2e6cff013cadb2347a4ef9a0
Derric Donehoo <derric.donehoo@gmail.com>
2019-06-04 17:07:12 +0200

    fix tzpo

--- app/services/shortener_service.rb
+++ app/services/shortener_service.rb
@@ -4,10 +4,9 @@ class ShortenerService
   end
 
   def call
-    @event.link
     response = HTTP.get "https://api.rebrandly.com/v1/links/new?apikey=#{ENV['REBRANDLY']}&destination=#{@event.link}&domain[fullName]=l.clubster.io"
     body = JSON.parse(response.to_s)
-    url = body['shorturl']
+    url = body['shortUrl']
     @event.update!(shorturl: url)
   end
 end
