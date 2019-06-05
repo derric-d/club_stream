412c12213e119fdffe49d78d19dd686c7cadc24b
Derric Donehoo <derric.donehoo@gmail.com>
2019-06-05 10:32:55 +0200

    send messages

--- app/jobs/sendmessages_job.rb
+++ app/jobs/sendmessages_job.rb
@@ -2,11 +2,12 @@ class SendmessagesJob < ApplicationJob
   queue_as :default
 
   def perform
-    if Date.today.tuesday?
-      User.all.each do |user|
+    # change date.today to tuesday for production
+    # if Date.today.wednesday?
+      User.find_each do |user|
         new_message = CreateMessagesService.new
         new_message.create_message(user)
       end
-    end
+    # end
   end
 end
