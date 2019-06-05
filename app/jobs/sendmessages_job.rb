3276c6fa31a17b5eb869ed1779cf59f7149ba5b8
Stephan Grund <stephan.grund@whu.edu>
2019-06-05 13:43:16 +0200

    message with image

--- app/jobs/sendmessages_job.rb
+++ app/jobs/sendmessages_job.rb
@@ -2,7 +2,7 @@ class SendmessagesJob < ApplicationJob
   queue_as :default
 
   def perform
-    if Date.today.tuesday?
+    if Date.today.wednesday?
       User.all.each do |user|
         new_message = CreateMessagesService.new
         new_message.create_message(user)
