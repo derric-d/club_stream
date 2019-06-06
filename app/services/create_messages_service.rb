5ad96c866db0150ea0f80fbf2a6ae177e9718516
Derric Donehoo <derric.donehoo@gmail.com>
2019-06-06 10:55:29 +0200

    space msgs

--- app/services/create_messages_service.rb
+++ app/services/create_messages_service.rb
@@ -22,16 +22,16 @@ class CreateMessagesService
         text: message,
         msisdn: "#{user.phone}"
       }
-    )
+      )
     Message.create(user_id: user.id, content: message)
     puts "#{message} sent to #{user.email}"
   end
 
- def event_message_builder(events)
-   events.map do |event|
-     if event.date > Date.today
-       "✔️ *#{event.name}* at #{event.club_name} on #{event.date.strftime("%a, %b %d")}, #{event.shorturl}"
-     end
-   end.join("\n\n")
- end
+  def event_message_builder(events)
+    events.map do |event|
+      if event.date > Date.today
+        "✔️ *#{event.name}* at #{event.club_name} on #{event.date.strftime("%a, %b %d")}, #{event.shorturl}"
+      end
+    end.join("\n\n")
+  end
 end
