3960211c6ef9f9c30b78e3fea1c8f67a02139d6b
Derric Donehoo <derric.donehoo@gmail.com>
2019-06-06 17:02:17 +0200

    Revert "pitch changes"
    This reverts commit b7294e261ccf863bc4b101eb22d73cc158bdc876.
    'revert to last stable'

--- app/javascript/plugins/typed.js
+++ app/javascript/plugins/typed.js
@@ -2,6 +2,6 @@ import Typed from 'typed.js';
 
 var typed = new Typed('.typed', {
   strings: ["Techno", "HipHop", "Pop", "90ies", "Disco", "Never miss a party","Clubster"],
-  typeSpeed: 130,
+  typeSpeed: 95,
   loop: true
 });
